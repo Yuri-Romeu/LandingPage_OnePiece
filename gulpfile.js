1c5888707d6d38cba0d486a02ddb3eedaec055f3
Yuri Romeu <yuriromeu2007@gmail.com>
2025-06-09 09:27:04 -0300

    começo projeto e gulp e sass instalado

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const htmlmin = require('gulp-htmlmin');
+
+function scripts() {
+     return gulp.src('./src/scripts/*.js').pipe(uglify()).pipe(gulp.dest('./dist/js'));
+}
+
+function styles() {
+     return gulp
+          .src('./src/styles/*.scss')
+          .pipe(sass({ outputStyle: 'compressed' }))
+          .pipe(gulp.dest('./dist/styles/'));
+}
+
+function images() {
+     return gulp
+          .src('./src/images/**/*', { encoding: false })
+          .pipe(imagemin())
+          .pipe(gulp.dest('./dist/images'));
+}
+
+function html() {
+     return gulp
+          .src('./src/*.html')
+          .pipe(htmlmin({ collapseWhitespace: true }))
+          .pipe(gulp.dest('./dist/'));
+}
+
+exports.default = gulp.parallel(styles, images, scripts, html);
+
+exports.watch = function () {
+     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
+     gulp.watch('./src/*.html', gulp.parallel(html));
+};
